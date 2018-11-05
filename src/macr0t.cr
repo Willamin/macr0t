488cf93e12c830faec99177eb88b6ff540254278
Will Lewis <will@wflewis.com>
2018-11-05 12:03:08 -0500

    Make it work

--- src/macr0t.cr
+++ src/macr0t.cr
@@ -1,3 +1,147 @@
 module Macr0t
   VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
+
+  PREFIXES = %w(
+    a
+    Afro
+    after
+    ambi
+    amphi
+    an
+    ana
+    Anglo
+    ante
+    anti
+    apo
+    arch
+    astro
+    auto
+    back
+    be
+    bi
+    bio
+    by
+    circum
+    cis
+    co
+    con
+    contra
+    counter
+    cryo
+    crypto
+    de
+    demi
+    demo
+    deuter
+    di
+    dia
+    dis
+    down
+    du
+    eco
+    electro
+    en
+    epi
+    Euro
+    ex
+    extra
+    fore
+    Franco
+    gain
+    geo
+    gyro
+    hemi
+    hetero
+    hind
+    homo
+    hydro
+    hyper
+    hypo
+    ideo
+    idio
+    in
+    Indo
+    infra
+    inter
+    intra
+    iso
+    macr
+    mal
+    maxi
+    mega
+    meta
+    micro
+    mid
+    midi
+    mini
+    mis
+    mono
+    multi
+    neo
+    non
+    ob
+    off
+    omni
+    on
+    ortho
+    out
+    over
+    paleo
+    pan
+    para
+    ped
+    per
+    peri
+    photo
+    pleo
+    pod
+    poly
+    post
+    pre
+    preter
+    pro
+    pros
+    proto
+    pseudo
+    pyro
+    quasi
+    re
+    retro
+    self
+    semi
+    socio
+    step
+    sub
+    super
+    supra
+    sur
+    syn
+    tele
+    trans
+    tri
+    twi
+    ultra
+    umbe
+    un
+    under
+    uni
+    up
+    vice
+    with
+    y
+  )
+
+  NUMBERS = (0..9).to_a.map(&.to_s)
+
+  LETTERS = ('a'..'z').to_a.map(&.to_s)
 end
+
+[
+  Macr0t::PREFIXES,
+  Macr0t::NUMBERS,
+  Macr0t::LETTERS,
+].each do |list|
+  print list.shuffle.first
+end
+
+puts
