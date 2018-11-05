d4a42d1f978ce0030749f1ef8fb4b3734e2cfbf7
Will Lewis <will@wflewis.com>
2018-11-05 12:02:08 -0500

    :sparkles: initial formation

---
+++ spec/spec_helper.cr
@@ -0,0 +1,4 @@
+require "../src/macr0t"
+require "microtest"
+include Microtest::DSL
+Microtest.run!
---
+++ src/macr0t.cr
@@ -0,0 +1,3 @@
+module Macr0t
+  VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
+end
